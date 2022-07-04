fb957dbeca84551d673e5fb528319dc715aef3df
LxnChan <LingXuanNing@Outlook.com>
2022-07-04 18:26:39 +0800

    Changes

---
+++ gocode/main.go
@@ -0,0 +1,15 @@
+//define package main
+//注释不能嵌套使用
+package main
+
+//import packages
+import "fmt"
+
+//func-定义一个函数
+//main-必须包含，是程序的入口函数
+//花括号的开始不能单独一行
+//逻辑必须在函数内部
+func main() {
+	//引用fmt包，引用里面的函数，在末尾自动添加ln（换行）
+	fmt.Println("Hello.")
+}
